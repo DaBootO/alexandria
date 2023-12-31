30cbc2e367f980077a28f40920a06af82d10b510
Dario Contrino <dariocontrino@yahoo.de>
2023-12-31 03:17:48 +0100

    added reactivity for color

--- alexandria-frontend/src/lib/Plot_2d.svelte
+++ alexandria-frontend/src/lib/Plot_2d.svelte
@@ -1,6 +1,8 @@
 <script>
+    // TODO: Maybe switch to Chart.js?
     import * as d3 from 'd3';
     export let data;
+    export let color_array;
     export let width = 640;
     export let height = 400;
     export let marginTop = 20;
@@ -30,14 +32,17 @@
     $: d3.select(gx).call(d3.axisBottom(x));
 </script>
 
+{@debug color_array}
 <svg {width} {height}>
     <g bind:this={gx} transform="translate(0,{height - marginBottom})" />
     <g bind:this={gy} transform="translate({marginLeft},0)" />
     {#each formattedData as d, i}
         <path fill="none" stroke="currentColor" stroke-width="3.5" d={line(d)} />
-        <g fill="white" stroke="#00FF00" stroke-width="1.5">
-            {#each d as point, j}
-                <circle key={i + '-' + j} cx={x(point.x)} cy={y(point.y)} r="2.5" />
+    {/each}
+    {#each data as djson}
+        <g fill={color_array[djson.uuid]} stroke={color_array[djson.uuid]} stroke-width="2.5">
+            {#each djson.x as point, i}
+                <circle cx={x(djson.x[i])} cy={y(djson.y[i])} r="2.5" />
             {/each}
         </g>
     {/each}
