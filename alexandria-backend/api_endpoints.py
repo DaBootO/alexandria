77fe9a684f61ca4d79d98c9ed26721a16dc005de
KianAhi <kianahi@hotmail.de>
2024-01-02 19:41:55 +0100

    Moved API endpoints and functions to seperate files

---
+++ alexandria-backend/api_endpoints.py
@@ -0,0 +1,39 @@
+import asyncio
+from aioch import Client
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Depends
+
+from clickhouse_functions import list_databases
+
+app = FastAPI()
+CLIENT = Client('localhost')
+
+
+@app.get("/")
+async def root():
+    return {"message": "Server is Running"}
+
+@app.get("/listDatabases")
+async def list_databases_endpoint(
+    client = None,
+    like: Optional[str] = None,
+    ilike: Optional[str] = None,
+    limit: Optional[int] = None,
+    outfile: Optional[str] = None,
+    format: Optional[str] = None,
+):
+    if client is None:
+        client = CLIENT
+    return await {"databases": list_databases(client, like, ilike, limit, outfile, format)}")
+
+@app.post("/createDatabase")
+async def create_database_endpoint(
+    client = None,
+    db_name: str,
+    cluster: Optional[str] = None,
+    engine: Optional[str] = None,
+    comment: Optional[str] = None,
+):
+    if client is None:
+        client = CLIENT
+    return await create_database(client, db_name, cluster, engine, comment)
